7ad65123039ec606f94574306c4ef8077aff03f9
Thiago <t.rocha@tafmobile.de>
2019-09-19 20:55:07 +0200

    Criação do projeto java-11/maven/spring-boot

---
+++ src/main/java/io/tmr/videostatistics/VideoStatisticsApplication.java
@@ -0,0 +1,13 @@
+package io.tmr.videostatistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoStatisticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoStatisticsApplication.class, args);
+	}
+
+}
