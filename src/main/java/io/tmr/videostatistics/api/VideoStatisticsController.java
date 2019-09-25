a15e1dd891d70dc74f2ae1fcb2fb5fc10b3e2aa6
Thiago <t.rocha@tafmobile.de>
2019-09-25 17:02:13 +0200

    remoção do contexto incorreto

--- src/main/java/io/tmr/videostatistics/api/VideoStatisticsController.java
+++ src/main/java/io/tmr/videostatistics/api/VideoStatisticsController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
@@ -23,9 +22,6 @@ import io.tmr.videostatistics.utils.DateUtils;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController()
-@RequestMapping({
-	"tmr"
-})
 @Slf4j
 public class VideoStatisticsController {
 
